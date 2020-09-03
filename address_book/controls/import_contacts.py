a5e81e1537391c3e27fe10c12d20b1b40010eeca
Pavel Zizka <pzizka@apple.com>
2020-09-03 10:04:31 +0100

    fixing import

--- address_book/controls/import_contacts.py
+++ address_book/controls/import_contacts.py
@@ -5,7 +5,9 @@ import jsonpickle
 def import_address_book(g_address_book):
     with open('exported_address_book.json') as json_file:
         json_data = json.load(json_file)
-        g_address_book = jsonpickle.decode(json_data)
-    print("Address book was imported.")
+        address_book = jsonpickle.decode(json_data)
 
-    return g_address_book
+        for contact in address_book:
+            g_address_book.append(contact)
+
+    print("Address book was imported.")
